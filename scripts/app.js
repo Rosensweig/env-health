aea86f13bf3ca582d89aacf031ae49dfb4640e82
Rosensweig <daniel.rosensweig@gmail.com>
2016-11-28 11:06:52 -0500

    with bugs

--- scripts/app.js
+++ scripts/app.js
@@ -22,9 +22,11 @@ $(document).ready(function() {
 });
 
 function getData(query, callback) {
+	/*
 	var settings = {
 		url: state.apis.airNow.BASE_URL,
-		dataType: 'json',
+		dataType: 'jsonp',
+		crossDomain: true,
 		type: 'GET',
 		success: callback,
 		processData: false,
@@ -35,8 +37,44 @@ function getData(query, callback) {
 			API_KEY: state.apis.airNow.API_KEY
 		})
 	};
-	$.ajax(settings);
+	console.log($.ajax(settings));
+	console.log("end of getData function");
+	*/
+	//var params = {
+
+		// data: makeQS({
+		// 	format: "application/json",
+		// 	zipCode: query,
+		// 	distance: 25,
+		// 	API_KEY: state.apis.airNow.API_KEY
+		// });
+        //  };
+            var result = $.ajax({
+                /* update API end point */
+                url: state.apis.airNow.BASE_URL,
+                data: makeQS({
+					format: "application/json",
+					zipCode: query,
+					distance: 25,
+					API_KEY: state.apis.airNow.API_KEY
+				}),
+                crossDomain: true,
+				success: callback,
+				processData: false,
+                dataType: "jsonp",
+                /*set the call type GET / POST*/
+                type: "GET"
+            })
+            /* if the call is NOT successful show errors */
+            .fail(function (jqXHR, error, errorThrown) {
+                console.log(jqXHR);
+                console.log(error);
+                console.log(errorThrown);
+            });
 }
+
+
+
 
 function makeQS(obj) {
 	var params = [];
@@ -47,5 +85,6 @@ function makeQS(obj) {
 }
 
 function displayData(data) {
+	console.log("Hello from displayData()");
 	console.log(data);
 }
